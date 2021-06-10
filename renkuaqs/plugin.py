77825105b8c9eb9e566c93aaa6f600af3e9ce14b
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-06-10 17:49:27 +0200

    small adjuistment

--- renkuaqs/plugin.py
+++ renkuaqs/plugin.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 #
+# Copyright 2020 - Viktor Gal
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,22 +24,17 @@ import rdflib
 from copy import deepcopy
 from pathlib import Path
 
-from renku.core.commands.client import pass_local_client
-from renku.core.commands.graph import Graph
-from renku.core.commands.format.graph import _conjunctive_graph
 from renku.core.models.cwl.annotation import Annotation
+from renku.core.incubation.command import Command
 from renku.core.plugins import hookimpl
-try:
-    from renku.core.models.provenance.provenance_graph import ProvenanceGraph
-    PG_AVAILABLE = True
-except:
-    PG_AVAILABLE = False
+from renku.core.models.provenance.provenance_graph import ProvenanceGraph
+from renku.core.errors import RenkuException
 
 from prettytable import PrettyTable
 from deepdiff import DeepDiff
 
-from .config import AQS_DIR
-#from aqsconverters.models import Run
+from aqsconverters.models import Run
+from aqsconverters.io import AQS_DIR, COMMON_DIR
 
 
 class AQS(object):
@@ -43,9 +42,9 @@ class AQS(object):
         self.run = run
 
     @property
-    def renku_AQS_path(self):
+    def renku_aqs_path(self):
         """Return a ``Path`` instance of Renku AQS metadata folder."""
-        return Path(self.run.client.renku_home).joinpath(AQS_DIR)
+        return Path(self.run.client.renku_home).joinpath(AQS_DIR).joinpath(COMMON_DIR)
 
     def load_model(self, path):
         """Load AQS reference file."""
@@ -59,117 +58,148 @@ def process_run_annotations(run):
     """``process_run_annotations`` hook implementation."""
     aqs = AQS(run)
 
-    for p in run.paths:
-        if p.startswith(str(aqs.renku_aqs_path)):
-            path = Path(p)
-            annotation_id = "{activity}/annotations/aqs/{id}".format(
-                activity=run._id, id=path.parts[-2]
-            )
-            return [
-                Annotation(
-                    id=annotation_id,
-                    source="aqs plugin",
-                    body=aqs.load_model(path)
-                )
-            ]
+    #os.remove(os.path.join(aqs.renku_aqs_path, "site.py"))
+    os.remove("sitecustomize.py")
+    
+    annotations = []
 
-    return []
+    if os.path.exists(aqs.renku_aqs_path):
+        for p in aqs.renku_aqs_path.iterdir():
+            aqs_annotation = aqs.load_model(p)
+            model_id = aqs_annotation["@id"]
+            annotation_id = "{activity}/annotations/aqs/{id}".format(
+                activity=run._id, id=model_id
+            )
+            p.unlink()
+            annotations.append(
+                Annotation(id=annotation_id, source="AQS plugin", body=aqs_annotation)
+            )
+    else:
+        print("nothing to process in process_run_annotations")
+
+    os.removedirs(".renku/aq/")
+
+    return annotations
+
+@hookimpl
+def pre_run(tool):
+    # we print
+    print(f"\033[31mhere we will prepare hooks for astroquery, tool given is {tool}\033[0m")    
+
+    # where to get renku.client?
+    os.makedirs(".renku/aq", exist_ok=True)
+
+    open("sitecustomize.py", "w").write("""
+print(f"\033[31mHERE enable hooks for astroquery\033[0m")    
+""")
+
 
 
 def _run_id(activity_id):
     return str(activity_id).split("/")[-1]
 
 
-def _graph(client, revision, paths):
-    if PG_AVAILABLE:
-        provenance_graph = ProvenanceGraph.from_json(client.provenance_graph_path)
-        provenance_graph.custom_bindings = [
-            #("aqs", "http://www.w3.org/ns/aqs#"), # will move there later
-            ("aqs", "http://odahub.io/ontology#"),
-            ("oa", "http://www.w3.org/ns/oa#"),
-            ("xsd", "http://www.w3.org/2001/XMLSchema#")
-        ]
-        return provenance_graph
+def _load_provenance_graph(client):
+    if not client.provenance_graph_path.exists():
+        raise RenkuException(
+            """Provenance graph has not been generated!
+Please run 'renku graph generate' to create the project's provenance graph
+"""
+        )
+    return ProvenanceGraph.from_json(client.provenance_graph_path)
 
-    renku_graph = Graph(client)
-    renku_graph.build(paths=paths, revision=revision)
-    cg = _conjunctive_graph(renku_graph)
 
-    cg.bind("aqs", "http://odahub.io/ontology#")
-    cg.bind("prov", "http://www.w3.org/ns/prov#")
-    cg.bind("oa", "http://www.w3.org/ns/oa#")
-    cg.bind("schema", "http://schema.org/")
-    cg.bind("xsd", "http://www.w3.org/2001/XMLSchema#")
-    return cg
+def _graph(revision, paths):
+    # FIXME: use (revision, paths) filter
+    cmd_result = Command().command(_load_provenance_graph).build().execute()
+
+    provenance_graph = cmd_result.output
+    provenance_graph.custom_bindings = {
+        "aqs": "http://www.w3.org/ns/aqs#",
+        "oa": "http://www.w3.org/ns/oa#",
+        "xsd": "http://www.w3.org/2001/XAQSchema#",
+    }
+    return provenance_graph
 
 
 def _create_leaderboard(data, metric, format=None):
     leaderboard = PrettyTable()
-    leaderboard.field_names = ["Run ID", "Model", "Inputs", metric]
-    leaderboard.align["Model"] = 'l'
-    leaderboard.align["Inputs"] = 'l'
-    leaderboard.align[metric] = 'r'
+    leaderboard.field_names = ["Run ID", "Module", "Query", metric]
+    leaderboard.align["Module"] = "l"
+    leaderboard.align["Query"] = "l"
+    leaderboard.align[metric] = "r"
     for commit, v in data.items():
         if metric in v:
-            v['inputs'].sort()
-            leaderboard.add_row([commit, v["model"], v['inputs'], v[metric]])
+            v["query"].sort()
+            leaderboard.add_row([commit, v["module"], v["query"], v[metric]])
     leaderboard.sortby = metric
     leaderboard.reversesort = True
     return leaderboard
 
 
 @click.group()
-def aqs()):
+def aqs():
     pass
 
 
 @aqs.command()
-@click.option("--revision", default="HEAD", help="The git revision to generate the log for, default: HEAD")
+@click.option(
+    "--revision",
+    default="HEAD",
+    help="The git revision to generate the log for, default: HEAD",
+)
 @click.option("--format", default="ascii", help="Choose an output format.")
 @click.option("--metric", default="accuracy", help="Choose metric for the leaderboard")
 @click.argument("paths", type=click.Path(exists=False), nargs=-1)
-@pass_local_client()
-def leaderboard(client, revision, format, metric, paths):
-    """Leaderboard based on evaluation metrics of machine learning models"""
-    graph = _graph(client, revision, None)
+def leaderboard(revision, format, metric, paths):
+    """Leaderboard based on performance of astroquery requests"""
+    graph = _graph(revision, paths)
     leaderboard = dict()
-    for r in graph.query("""SELECT DISTINCT ?type ?value ?run ?runId ?dsPath where {{
+    for r in graph.query(
+        """SELECT DISTINCT ?type ?value ?run ?runId ?dsPath where {{
         ?em a aqs:ModelEvaluation ;
         aqs:hasValue ?value ;
         aqs:specifiedBy ?type ;
         ^aqs:hasOutput/aqs:implements/rdfs:label ?run ;
         ^aqs:hasOutput/^oa:hasBody/oa:hasTarget ?runId ;
         ^aqs:hasOutput/^oa:hasBody/oa:hasTarget/prov:qualifiedUsage/prov:entity/prov:atLocation ?dsPath
-        }}"""):
+        }}"""
+    ):
         run_id = _run_id(r.runId)
         metric_type = r.type.split("#")[1]
         if run_id in leaderboard:
-            leaderboard[run_id]['inputs'].append(r.dsPath.__str__())
+            leaderboard[run_id]["inputs"].append(r.dsPath.__str__())
             continue
         leaderboard[run_id] = {
-                metric_type: r.value.value,
-                "model": r.run,
-                "inputs": [r.dsPath.__str__()]
-            }
+            metric_type: r.value.value,
+            "model": r.run,
+            "inputs": [r.dsPath.__str__()],
+        }
     if len(paths):
         filtered_board = dict()
         for path in paths:
-            filtered_board.update(dict(filter(
-                lambda x: path in x[1]["inputs"], leaderboard.items()
-            )))
+            filtered_board.update(
+                dict(filter(lambda x: path in x[1]["inputs"], leaderboard.items()))
+            )
         print(_create_leaderboard(filtered_board, metric))
     else:
         print(_create_leaderboard(leaderboard, metric))
 
 
 @aqs.command()
-@click.option("--revision", default="HEAD", help="The git revision to generate the log for, default: HEAD")
+@click.option(
+    "--revision",
+    default="HEAD",
+    help="The git revision to generate the log for, default: HEAD",
+)
 @click.option("--format", default="ascii", help="Choose an output format.")
-@click.option("--diff", nargs=2, help="Print the difference between two model revisions")
+@click.option(
+    "--diff", nargs=2, help="Print the difference between two model revisions"
+)
 @click.argument("paths", type=click.Path(exists=False), nargs=-1)
-@pass_local_client()
-def params(client, revision, format, paths, diff):
-    """List the hyper-parameter settings of machine learning models"""
+def params(revision, format, paths, diff):
+    """List the parameters of astroquery requests"""
+
     def _param_value(rdf_iteral):
         if not type(rdf_iteral) != rdflib.term.Literal:
             return rdf_iteral
@@ -178,10 +208,10 @@ def params(client, revision, format, paths, diff):
         else:
             return rdf_iteral.toPython()
 
-
-    graph = _graph(client, revision, paths)
+    graph = _graph(revision, paths)
     model_params = dict()
-    for r in graph.query("""SELECT ?runId ?algo ?hp ?value where {{
+    for r in graph.query(
+        """SELECT ?runId ?algo ?hp ?value where {{
         ?run a aqs:Run ;
         aqs:hasInput ?in .
         ?in a aqs:HyperParameterSetting .
@@ -189,15 +219,15 @@ def params(client, revision, format, paths, diff):
         ?in aqs:hasValue ?value .
         ?run aqs:implements/rdfs:label ?algo ;
         ^oa:hasBody/oa:hasTarget ?runId
-        }}"""):
+        }}"""
+    ):
         run_id = _run_id(r.runId)
         if run_id in model_params:
             model_params[run_id]["hp"][str(r.hp)] = _param_value(r.value)
         else:
-            model_params[run_id] = dict({
-                "algorithm": str(r.algo),
-                "hp": {str(r.hp): _param_value(r.value)}
-            })
+            model_params[run_id] = dict(
+                {"algorithm": str(r.algo), "hp": {str(r.hp): _param_value(r.value)}}
+            )
 
     if len(diff) > 0:
         for r in diff:
@@ -209,27 +239,31 @@ def params(client, revision, format, paths, diff):
             print("\t- {}".format(model_params[diff[0]]["algorithm"]))
             print("\t+ {}".format(model_params[diff[1]]["algorithm"]))
         else:
-            params_diff = DeepDiff(model_params[diff[0]], model_params[diff[1]], ignore_order=True)
+            params_diff = DeepDiff(
+                model_params[diff[0]], model_params[diff[1]], ignore_order=True
+            )
             output = PrettyTable()
             output.field_names = ["Hyper-Parameter", "Old", "New"]
-            output.align["Hyper-Parameter"] = 'l'
+            output.align["Hyper-Parameter"] = "l"
             if "values_changed" not in params_diff:
                 print(output)
                 return
             for k, v in params_diff["values_changed"].items():
                 parameter_name = re.search(r"\['(\w+)'\]$", k).group(1)
-                output.add_row([
-                    parameter_name,
-                    _param_value(v['new_value']),
-                    _param_value(v['old_value'])
-                ])
+                output.add_row(
+                    [
+                        parameter_name,
+                        _param_value(v["new_value"]),
+                        _param_value(v["old_value"]),
+                    ]
+                )
             print(output)
     else:
         output = PrettyTable()
         output.field_names = ["Run ID", "Model", "Hyper-Parameters"]
-        output.align["Run ID"] = 'l'
-        output.align["Model"] = 'l'
-        output.align["Hyper-Parameters"] = 'l'
+        output.align["Run ID"] = "l"
+        output.align["Model"] = "l"
+        output.align["Hyper-Parameters"] = "l"
         for runid, v in model_params.items():
             output.add_row([runid, v["algorithm"], json.dumps(v["hp"])])
         print(output)
