7f0f6fb664eb0cb321daf1f4fd22bb36eae898ff
burnout87 <gabriele.barni@gmail.com>
2023-02-16 11:24:03 +0100

    check also including the newline character

--- renkuaqs/javascript_graph_utils.py
+++ renkuaqs/javascript_graph_utils.py
@@ -6,8 +6,7 @@ def gitignore_file(file_name):
     if os.path.exists('.gitignore'):
         with open('.gitignore') as gitignore_file_lines:
             lines = gitignore_file_lines.readlines()
-        print(f"lines: {lines}")
-        if file_name not in lines:
+        if file_name + "\n" not in lines:
             lines.append(file_name + "\n")
             with open(".gitignore", "w") as gitignore_file_write:
                 gitignore_file_write.writelines(lines)
