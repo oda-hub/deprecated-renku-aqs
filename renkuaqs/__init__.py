6a482662ecee19b49b7fa04d7b307d13bbd57513
burnout87 <gabriele.barni@gmail.com>
2023-02-16 08:27:03 +0100

    check for errors when generating html

--- renkuaqs/__init__.py
+++ renkuaqs/__init__.py
@@ -56,13 +56,12 @@ class GetGraphHandler(SimpleHTTPRequestHandler):
         mount_path_env = os.environ.get('MOUNT_PATH', None)
         logging.info(f'self.path = {self.path}, os.cwd = {os.getcwd()}, mount_path = {mount_path_env}')
         if self.path == '/':
-            aqsPlugin.build_graph(paths=os.getcwd(), template_location="remote")
-            self.path = 'graph.html'
-            # if os.path.exists(os.path.join(os.getcwd(), 'graph.html')):
-            #     if 'MOUNT_PATH' in os.environ:
-            #         self.path = os.path.join(os.environ['MOUNT_PATH'], 'graph.html')
-            #     else:
-            #         self.path = 'graph.html'
+            try:
+                aqsPlugin.build_graph(paths=os.getcwd(), template_location="remote")
+            except Exception as e:
+                logging.warning(f"Error whiel generating the output graph: {e}")
+            if os.path.exists('graph.html'):
+                self.path = 'graph.html'
 
         if self.path == '/lib/bindings/utils.js':
             pyvis_package_path = pyvis.__path__[0]
