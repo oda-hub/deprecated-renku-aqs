5dd4122c9f91e1c7abadd1df21c1de06a38c4c3b
burnout87 <gabriele.barni@gmail.com>
2023-02-27 17:46:21 +0100

    icon refresh button, hasTarget to calls, no need for hasTarget entry graphical configuration

--- renkuaqs/javascript_graph_utils.py
+++ renkuaqs/javascript_graph_utils.py
@@ -32,11 +32,16 @@ def set_html_content(net,
 
     html_code = '''
         <div style="margin: 5px 0px 15px 5px">
-            <button type="button" class="btn btn-secondary btn-sm" onclick="refresh_graph()">Refresh graph!</button>
-            <button type="button" class="btn btn-secondary btn-sm" onclick="reset_graph()">Reset graph!</button>
-            <button type="button" class="btn btn-secondary btn-sm" onclick="fit_graph()">Fit graph!</button>
-            <button type="button" class="btn btn-secondary btn-sm" onclick="stop_animation()">Stop animation!</button>
-            <button type="button" class="btn btn-secondary btn-sm collapsible" onclick="open_ttl_content()">Display ttl content!</button> \
+            <button class="btn btn-secondary btn-sm" onclick="refresh_graph()" type="button">
+             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                 <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                 <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+               </svg>
+            </button>
+            <button type="button" class="btn btn-secondary btn-sm" onclick="reset_graph()">Reset graph</button>
+            <button type="button" class="btn btn-secondary btn-sm" onclick="fit_graph()">Fit graph</button>
+            <button type="button" class="btn btn-secondary btn-sm" onclick="stop_animation()">Stop animation</button>
+            <button type="button" class="btn btn-secondary btn-sm collapsible" onclick="open_ttl_content()">Display ttl content</button> \
                  \n<div id="ttl_content"></div>
         </div>
         <div style="display:flex;">
